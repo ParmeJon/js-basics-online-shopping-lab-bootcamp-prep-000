8fec215727740af11f636009dfc3da1e05b0c257
Jonathan Chan <jonchan816@gmail.com>
2018-10-11 10:00:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -52,9 +52,11 @@ function total() {
 function removeFromCart(item) {
   // write your code here
   for (var i = 0; i < cart.length; i++) {
-    if (cart[i].nameItem = item) {
-      cart.splice(i, 1)
-    }
+    if (cart[i].nameItem === item) {
+      cart.splice(i, 1);
+      return cart;
+    } 
+    
   }
 }
 
