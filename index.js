4be915b9a2f8a3e42894d00e5481eb37316614e8
Jonathan Chan <jonchan816@gmail.com>
2018-10-11 08:31:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var cart = [];
+var price = Math.floor(Math.random() * 100) + 1;
 
 function getCart() {
  return cart;
@@ -11,11 +12,27 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
+ var items = {itemName:item};
+ var price = Math.floor(Math.random() * 100) + 1;
+ var prices = {itemPrice:price};
+ const totalItem = Object.assign(items, prices);
+ cart.push(totalItem);
+ return `${item} has been added to your cart.`
 }
 
 function viewCart() {
   // write your code here
-}
+  if (cart.length === 0) {
+    return "Your shopping cart is empty."
+  } else var sentenceFinish = []; { 
+    for (var i = 0; i < cart.length; i++) {
+    for (var item in cart[i]){
+      sentenceFinish.push(`${item[0]} at $${cart[i]}`)
+    }
+   return `In your cart, you have ${sentenceFinish.join(', ')}.`
+   }
+  }
+  }
 
 function total() {
   // write your code here
